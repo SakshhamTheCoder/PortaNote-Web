b5eb13366e9e0cb2a0b4f500cea65ae59ce478eb
SakshhamTheCoder <sakshhamtg@gmail.com>
2024-01-01 15:35:20 +0530

    Update mobile page

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,13 +20,10 @@ export default function RootLayout({ children }) {
                     <>
                         <main className="flex min-h-screen text-center">
                             <div className="flex flex-col items-center justify-center flex-1">
-                                <h1 className="text-6xl font-bold">Welcome to PortaNote</h1>
-                                <p className="mt-3 text-xl">A note-taking app accessible on the go</p>
-                                <div className="rounded-lg p-6 mt-5 bg-neutral-700 block max-w-sm w-screen">
-                                    <p className="text-2xl">
-                                        Please use a desktop browser to access this app or install the mobile app
-                                    </p>
-                                </div>
+                                <h1 className="text-6xl font-bold">PortaNote Web is only available on desktop</h1>
+                                <p className="mt-3 text-xl">
+                                    Please use a desktop browser to access this app or install the mobile app
+                                </p>
                             </div>
                         </main>
                     </>
